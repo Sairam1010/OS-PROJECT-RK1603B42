f86ffc736a5e7384fe216b69b772a3eabf4c9ded
fr05t99 <dwijrastogi100@gmail.com>
2018-04-04 17:04:55 +0530

    added calculate and find top priority

--- OSPROJECT.c
+++ OSPROJECT.c
@@ -20,6 +20,11 @@ void enqueue()
     scanf("%d", &(temp->arrivalTime));
     printf("\nEnter burst time : ");
     scanf("%d", &(temp->burstTime));
+    if ((temp->burstTime <= 0) || (temp->arrivalTime < 0))
+    {
+        printf("Wrong process details entered\n");
+        exit(0);
+    }
     temp->priority = 0;
     temp->waitTime = 0;
     temp->id = idT;
@@ -54,32 +59,42 @@ void show()
 }
 void find()
 {
+    //printf("find");
     temp = start;
-    chosen = start;
-    while (chosen->arrivalTime > totalTime)
-    {
-        chosen = chosen->link;
-    }
+    chosen = NULL;
     while (temp != NULL)
+    {
+        if (chosen == NULL)
         {
-            if ((temp->priority < chosen->priority) && (temp->arrivalTime >= totalTime))
+            if (temp->arrivalTime <= totalTime)
             {
                 chosen = temp;
             }
-            temp = temp->link;
         }
+        else
+        {
+            if ((chosen->priority > temp->priority) && (temp->arrivalTime <= totalTime))
+            {
+                chosen = temp;
+            }
+        }
+        temp = temp->link;
+    }
 }
 void findPriority()
 {
+    //printf("Prio");
     int i;
     temp = start;
-    for (i = 0;i < nOfPr;i++)
+    while(temp != NULL)
     {
         temp->priority = (1 + temp->waitTime) / temp->burstTime;
+        temp = temp->link;
     }
 }
-void remove()
+void removeProcess()
 {
+    //printf("Hey");
     if (chosen == start)
     {
         start = start = start->link;
@@ -95,14 +110,53 @@ void remove()
         chosen->link = NULL;
     }
 }
+void increaseWaiting(int cycles)
+{
+    temp = start;
+    while(temp != NULL)
+    {
+        if (temp->arrivalTime <= totalTime)
+        {
+            temp->waitTime += cycles;
+        }
+        temp = temp->link;
+    }
+}
 int main()
 {
    printf("Enter no. of processes( > 1) : ");
    scanf("%d",&nOfPr);
+   if (nOfPr <= 1)
+   {
+       printf("No. of processes is less than 2");
+       exit(0);
+   }
    int i;
    for (i = 0;i < nOfPr;i++)
    {
        enqueue();
    }
-
+    while (start != NULL)
+    {
+        //printf("Hi");
+        findPriority();
+        find();
+        //printf("Hii");
+        if (chosen == NULL)
+        {
+            //printf("Hiii");
+            totalTime++;
+            increaseWaiting(1);
+        }
+        else
+        {
+            int localBurst = chosen->burstTime;
+            totalTime += chosen->burstTime;
+            chosen->turnaround = totalTime - chosen->arrivalTime;
+            printf("\n\nProcess %d with \n\nWaiting time %d\n\nTurnaround time %d\n\nended at %d\n", chosen->id, chosen->waitTime,chosen->turnaround,totalTime);
+            removeProcess();
+            nOfPr--;
+            increaseWaiting(localBurst);
+        }
+    }
 }
